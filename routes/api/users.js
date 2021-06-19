70ff1076b53b104ae0dd800308ec2dc57293bcda
yash pawar <45911343+yashpwr@users.noreply.github.com>
2021-06-19 16:20:22 +0530

    Implementing JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-const {check, validationResult} = require('express-validator/check');
+const {check, validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -48,13 +50,25 @@ async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
 
-        //
+        // Save data to Database
         user.save();
 
         // return json web token
-        
-        res.send('User Registered');
+        const payload = {
+            user : {
+                id : user.id,
+            }
+        }
 
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expiresIn : 360000 },
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            }
+        )
     } catch (err) {
         console.log(err.message);
         res.status(500).send('Server Error');
