2b30d1f28c24d2ab354321db0d59d45f982610d2
yash pawar <45911343+yashpwr@users.noreply.github.com>
2021-06-19 15:18:05 +0530

    Route Files With Express Router

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const express = require('express');
 
 const connectDB = require('./config/db');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const auth = require('./routes/api/auth');
+
 const app = express();
 
 //COnnect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get('/', (req, res) => res.send('Hello World'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+app.use('/api/auth', auth);
 
 const PORT = process.env.PORT || 5000;
 
