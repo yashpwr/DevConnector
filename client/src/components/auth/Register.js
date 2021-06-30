b248510ecf89be9effb9efeca6879678903c7543
yash pawar <45911343+yashpwr@users.noreply.github.com>
2021-06-30 22:37:29 +0530

    Request Example & Login Form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, {Fragment, useState} from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Register = () => {
 
@@ -99,7 +101,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>;
 }
