0203b54d41a1b9a5c2137a30ecb16fa80d6e7d97
yash pawar <45911343+yashpwr@users.noreply.github.com>
2021-06-30 22:55:58 +0530

    setup redux and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,26 @@ import Navbar from './components/layout/navbar';
 import Landing from './components/layout/landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+
+//Reducx 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => 
-<Router>
-    <Fragment>
-      <Navbar/>
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-</Router>
+<Provider store={store}>
+  <Router>
+      <Fragment>
+        <Navbar/>
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+  </Router>
+</Provider>
 export default App;
